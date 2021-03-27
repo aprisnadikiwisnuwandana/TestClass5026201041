7f413a5f008cd1d91cfecf14d610fa30fc3fe782
aprisnadikiwisnuwandana <81064858+aprisnadikiwisnuwandana@users.noreply.github.com>
2021-03-28 01:29:24 +0700

    Add files via upload

---
+++ TestClass5026201041.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.lang.*;
+
+public class TestClass5026201041 {
+
+    public static void main(String[] args) {
+        ComputeMethods5026201041 result = new ComputeMethods5026201041();
+        result.fToC(100.4);
+        result.hypotenuse(3, 4);
+        result.roll();
+    }
+}
